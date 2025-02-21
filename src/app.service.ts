650b7500d5c8f30205f022c9e9df3a4dfa0129a7
Andrea Moguel K. <31602026+amoguelk@users.noreply.github.com>
2025-02-21 09:35:10 -0600

    Created Nest app

---
+++ src/app.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@nestjs/common';
+
+/**
+ * A basic service with a single method
+ */
+@Injectable()
+export class AppService {
+  getHello(): string {
+    return 'Hello World!';
+  }
+}
